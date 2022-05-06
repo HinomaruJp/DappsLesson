1c4ffec7db312efa8aac6abb12a63620c6bec4aa
HinomaruJp <104541171+HinomaruJp@users.noreply.github.com>
2022-05-06 14:55:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("hello world");
+
+async function myFunction() {
+ const provider = await new ethers.providers.web3Provider.(window.ethereum)
+ const blocknumber = await provider.getBlockNumber();
+ alert(blocknumber);
+}
+
+
+window.onload = function(){
+  myFunction();
+}
+
